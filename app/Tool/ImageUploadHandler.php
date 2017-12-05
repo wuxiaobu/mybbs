d4c1b5a6d6127bd9405d7bbfff3ead9f03d5d7cc
zhixing <zhixing@hashmeta.com>
2017-12-05 15:09:25 +0800

    添加分类

--- app/Tool/ImageUploadHandler.php
+++ app/Tool/ImageUploadHandler.php
@@ -42,7 +42,7 @@ class ImageUploadHandler
         }
 
         return [
-            'path' => config('app.url') . "/$folder_name/$filename"
+            'path' => "/$folder_name/$filename"
         ];
     }
 
