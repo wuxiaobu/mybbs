ad15ae09f8c67ff329acf1526a425357c6aebaf7
wuxiaobu <woshilieqie@163.com>
2017-12-08 15:51:07 +0800

    翻译slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use App\Tool\TranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -24,5 +25,10 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            $topic->slug = app(TranslateHandler::class)->translate($topic->title);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::get('topics/{topic}/destroy', 'TopicsController@destroy');
 
 
