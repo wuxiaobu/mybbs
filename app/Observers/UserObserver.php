1802f2cddffe8278c93950cce12776e311c14003
wuxiaobu <woshilieqie@163.com>
2017-12-22 17:18:52 +0800

    设置默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -12,6 +12,10 @@ class UserObserver
     public function saving(User $user)
     {
         $user->activation_token = str_random(30);
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = '/default-avatar.jpg';
+        }
     }
 
     public function updating(User $user)
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -49,7 +49,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img style="height:30px !important" src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" >
+                                <img style="height:30px !important" src="{{ asset(Auth::user()->avatar) }}" class="img-responsive img-circle" width="30px" >
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
